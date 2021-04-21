0c870d8f8af40972b22b0e6641140ef4de2937e2
Zoella2021 <2695969122@qq.com>
2021-04-21 15:53:59 +0800

    wyy 2nd 页面跳转

--- app/src/main/java/com/example/suijifront/MessageLogInActivity.java
+++ app/src/main/java/com/example/suijifront/MessageLogInActivity.java
@@ -2,13 +2,27 @@ package com.example.suijifront;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Button;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MessageLogInActivity extends AppCompatActivity {
-
+    private Button login4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_log_in);
+        login4=(Button) findViewById(R.id.login4);
+        login4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MessageLogInActivity.this, JitangActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
