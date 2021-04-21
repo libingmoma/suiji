f529088d755513b64460ece68bb7ec9886844b5e
yiyiyijiajiajia <10195101471@ecnu.edu.cn>
2021-04-21 15:06:46 +0800

    页面跳转

---
+++ app/src/main/java/com/example/suijifront/CsContentEditActivity.java
@@ -0,0 +1,14 @@
+package com.example.suijifront;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CsContentEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cs_content_edit);
+    }
+}
---
+++ app/src/main/java/com/example/suijifront/TodoEditActivity.java
@@ -0,0 +1,14 @@
+package com.example.suijifront;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodoEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_edit);
+    }
+}
