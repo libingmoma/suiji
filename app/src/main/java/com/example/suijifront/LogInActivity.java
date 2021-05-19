942e67b676250b39347a011666b7f6268923caeb
yiyiyijiajiajia <10195101471@ecnu.edu.cn>
2021-05-19 14:49:24 +0800

    zyj 2nd 今日课程主界面

--- app/src/main/java/com/example/suijifront/LogInActivity.java
+++ app/src/main/java/com/example/suijifront/LogInActivity.java
@@ -2,13 +2,26 @@ package com.example.suijifront;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
+    private Button login3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        login3=(Button) findViewById(R.id.login3);
+        login3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LogInActivity.this,JitangActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
