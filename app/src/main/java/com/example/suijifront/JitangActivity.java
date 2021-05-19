a20eaffa376bb45f95f6e549160d4cb54409cddc
Zoella2021 <2695969122@qq.com>
2021-05-19 15:18:58 +0800

    WYY 4th 鸡汤跳转和颜色添加

--- app/src/main/java/com/example/suijifront/JitangActivity.java
+++ app/src/main/java/com/example/suijifront/JitangActivity.java
@@ -2,13 +2,25 @@ package com.example.suijifront;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class JitangActivity extends AppCompatActivity {
+    private Button receive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jitang);
+        receive=(Button) findViewById(R.id.receive);
+        receive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(JitangActivity.this,CourseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
