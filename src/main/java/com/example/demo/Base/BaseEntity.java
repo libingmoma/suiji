6686699c3f0c046925914033aab20deaf294aa0e
hhp-724 <10195101465@stu.ecnu.edu.cn>
2021-05-27 18:14:49 +0800

    hhp修改关于返回数据多出一个id的问题

--- src/main/java/com/example/demo/Base/BaseEntity.java
+++ src/main/java/com/example/demo/Base/BaseEntity.java
@@ -1,22 +1,12 @@
 package com.example.demo.Base;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import java.io.Serializable;
 
 @Entity
 public class BaseEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    private String id;
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    @Id
-    public String getId() {
-        return id;
-    }
 
 
     /*@Transient
