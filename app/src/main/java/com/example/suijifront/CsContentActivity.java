c813377eebc173fe0b8363ca6344998295d41ad6
Zoella2021 <2695969122@qq.com>
2021-06-04 01:21:34 +0800

    wyy 8th 课程内容编辑

--- app/src/main/java/com/example/suijifront/CsContentActivity.java
+++ app/src/main/java/com/example/suijifront/CsContentActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class CsContentActivity extends AppCompatActivity {
 
     private ImageView cs_content_back;
+    private Button edit2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,15 @@ public class CsContentActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(CsContentActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        edit2=(Button) findViewById(R.id.edit2);
+        edit2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(CsContentActivity.this, CsContentEditActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/suijifront/CsContentEditActivity.java
+++ app/src/main/java/com/example/suijifront/CsContentEditActivity.java
@@ -2,13 +2,28 @@ package com.example.suijifront;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CsContentEditActivity extends AppCompatActivity {
+
+    private ImageView cs_content_edit_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cs_content_edit);
+
+        cs_content_edit_back=(ImageView) findViewById(R.id.cs_content_edit_back);
+        cs_content_edit_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(CsContentEditActivity.this, CsContentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
